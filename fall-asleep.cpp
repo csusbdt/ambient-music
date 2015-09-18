e0480ae72b3e077bcc90d945747cb05f63253853
David Turner <csusbdt@gmail.com>
2015-09-18 09:37:33 -0700

    X

--- fall-asleep.cpp
+++ fall-asleep.cpp
@@ -14,37 +14,37 @@ double sample(double t) {
 		sin(2 * PI * t * F / pow(PHI, -2)) *
 		(sin(2 * PI * t * T / pow(PHI, -2)) * .5 + .5) *  // waking 
 		(sin(2 * PI * t / (P * pow(PHI, -2)) - PI / 2) * .5 + .5) *
-		(sin(2 * PI * t / (duration * 2) + 3 / 3 * PI / 2) * .5 + .5)
+		(pow(sin(2 * PI * t / (duration * 2) + 3 / 3 * PI / 2), 5) * .5 + .5)
 		+
 		sin(2 * PI * t * F / pow(PHI, -1)) *
 		(sin(2 * PI * t * T / pow(PHI, -1)) * .5 + .5) *  // dream/waking transition
 		(sin(2 * PI * t / (P * pow(PHI, -1)) - PI / 2) * .5 + .5) *
-		(sin(2 * PI * t / (duration * 2) + 2 / 3 * PI / 2) * .5 + .5)
+		(pow(sin(2 * PI * t / (duration * 2) + 2 / 3 * PI / 2), 5) * .5 + .5)
 		+
 		sin(2 * PI * t * F / pow(PHI, 0)) *
 		(sin(2 * PI * t * T / pow(PHI, 0)) * .5 + .5) *  // dreaming boundary
 		(sin(2 * PI * t / (P * pow(PHI, 0)) - PI / 2) * .5 + .5) *
-		(sin(2 * PI * t / (duration * 2) + 1 / 3 * PI / 2) * .5 + .5)
+		(pow(sin(2 * PI * t / (duration * 2) + 1 / 3 * PI / 2), 5) * .5 + .5)
 		+
 		sin(2 * PI * t * F / pow(PHI, 1)) *
 		(sin(2 * PI * t * T / pow(PHI, 1)) * .5 + .5) *  // dreaming
 		(sin(2 * PI * t / (P * pow(PHI, 1)) - PI / 2) * .5 + .5) *
-		(sin(2 * PI * t / (duration * 2) + 0 / 3 * PI / 2) * .5 + .5)
+		(pow(sin(2 * PI * t / (duration * 2) + 0 / 3 * PI / 2), 5) * .5 + .5)
 		+
 		sin(2 * PI * t * F / pow(PHI, 2)) *
 		(sin(2 * PI * t * T / pow(PHI, 2)) * .5 + .5) *  // deep sleep boundary
 		(sin(2 * PI * t / (P * pow(PHI, 2)) - PI / 2) * .5 + .5) *
-		(sin(2 * PI * t / (duration * 2) - 1 / 3 * PI / 2) * .5 + .5)
+		(pow(sin(2 * PI * t / (duration * 2) - 1 / 3 * PI / 2), 5) * .5 + .5)
 		+
 		sin(2 * PI * t * F / pow(PHI, 3)) *
 		(sin(2 * PI * t * T / pow(PHI, 3)) * .5 + .5) *  // deep sleep
 		(sin(2 * PI * t / (P * pow(PHI, 3)) - PI / 2) * .5 + .5) *
-		(sin(2 * PI * t / (duration * 2) - 2 / 3 * PI / 2) * .5 + .5)
+		(pow(sin(2 * PI * t / (duration * 2) - 2 / 3 * PI / 2), 5) * .5 + .5)
 		+
 		sin(2 * PI * t * F / pow(PHI, 4)) *
 		(sin(2 * PI * t * T / pow(PHI, 4)) * .5 + .5) *  // deep sleep
 		(sin(2 * PI * t / (P * pow(PHI, 4)) - PI / 2) * .5 + .5) *
-		(sin(2 * PI * t / (duration * 2) - 3 / 3 * PI / 2) * .5 + .5);
+		(pow(sin(2 * PI * t / (duration * 2) - 3 / 3 * PI / 2), 5) * .5 + .5);
 	s /= 4;
 	assert(-1 <= s && s <= 1);
 	return s; 
