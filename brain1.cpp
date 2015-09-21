1359896b259bc5b806eb1c08154d2c2e24f1e35e
David Turner <csusbdt@gmail.com>
2015-09-20 18:51:30 -0700

    X

---
+++ brain1.cpp
@@ -0,0 +1,48 @@
+#include "main.inc"
+
+// Brain wave frequencies
+//   0.1 -  4 Hz  DELTA  dreamless sleep
+//     4 -  8 Hz  THETA  dream sleep
+//     8 - 14 Hz  ALPHA  transition between sleeping and waking states
+//    14 - 30 Hz  BETA   waking state
+
+const double F = 396;  // base tone frequency
+//const double F = 528; 
+const double T = 10;   // base tremolo frequency
+const double P = 12;   // base note period
+
+void init() {
+	rampUpTime = 0.0;
+	rampDownTime = 1.5;
+	duration = 9 * 60;
+}
+
+double ramp(double t, double t0, double a0, double t1, double a1) {
+	if (t < t0) return a0;
+	if (t > t1) return a1;
+	return (t - t0) / (t1 - t0) * a0 + (t1 - t) / (t1 - t0) * a1;
+}
+
+double sample(double t) {
+	double d = duration;
+	double s = 
+		sin(2 * PI * t * F / pow(PHI, 0)) *
+		(sin(2 * PI * t * T / pow(PHI, 0) - PI / 2) * .5 + .5) * 
+		(sin(2 * PI * t / (P * pow(PHI, 0)) - PI / 2) * .5 + .5)
+		+
+		sin(2 * PI * t * F / pow(PHI, 1)) *
+		(sin(2 * PI * t * T / pow(PHI, 0) - PI / 4) * .5 + .5) *  
+		(sin(2 * PI * t / (P * pow(PHI, 1)) - PI / 2) * .5 + .5)
+		+
+		sin(2 * PI * t * F / pow(PHI, 2)) *
+		(sin(2 * PI * t * T / pow(PHI, 0) + PI / 4) * .5 + .5) *  
+		(sin(2 * PI * t / (P * pow(PHI, 2)) - PI / 2) * .5 + .5)
+		+
+		sin(2 * PI * t * F / pow(PHI, 3)) *
+		(sin(2 * PI * t * T / pow(PHI, 0) + PI / 2) * .5 + .5) *  
+		(sin(2 * PI * t / (P * pow(PHI, 3)) - PI / 2) * .5 + .5);
+	s /= 64;
+	assert(-1 <= s && s <= 1);
+	return s; 
+}
+
