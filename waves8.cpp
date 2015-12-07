10c61fe01f45a659bf1c8f5af511d2848cb27983
David Turner <susbdt@gmail.com>
2015-12-06 17:50:29 -0800

    X

--- waves8.cpp
+++ waves8.cpp
@@ -1,6 +1,6 @@
 #include "main.inc"
 
-// waves7.cpp   ********************* UNFINISHED **********************
+// waves8.cpp   ********************* UNFINISHED **********************
 
 // See http://www.phy.mtu.edu/~suits/NoteFreqCalcs.html
 double scale(double octaveTones, double baseFrequency, double halfNotesAwayFromBase) {
@@ -24,30 +24,30 @@ double period(double t, double period, double normalizedPhase = 0, double min = 
 void init() {
 	rampUpTime = 1.0;
 	rampDownTime = 1.5;
-	duration = 16 * 60;
+	duration = 12 * 60;
 }
 
 double sample(double t) {
+	double F = 16;
+	double P = 6;
+
+	double BIG_P = duration;
+
 	double s1 = 0;
 
-	s1 += tone(t, 432) * freq(t, 8) * period(t, 10);
-	s1 += tone(t, 433) * freq(t, 8) * period(t, 11);
-	s1 += tone(t, 410) * freq(t, 5) * period(t, 19);
-	s1 += tone(t, 410.5) * period(t, 21);
+	s1 += tone(t, 180 * 1)   * freq(t, 16)       * period(t, 8);
+	s1 += tone(t, 180 * PHI) * freq(t, 16 * PHI) * period(t, 8 / PHI);
+	s1 += tone(t, 180 * 2)   * freq(t, 16 / PHI) * period(t, 8 * PHI);
 
 	double s2 = 0;
 
-	s2 += tone(t, 100) * freq(t, 8) * period(t, 8);
-	s2 += tone(t, 200) * freq(t, 6) * period(t, 12);
+	s2 += tone(t, 220 * 1)   * freq(t, 12)       * period(t, 11);
+	s2 += tone(t, 220 / PHI) * freq(t, 12 * PHI) * period(t, 11 / PHI);
+	s2 += tone(t, 220 * 2)   * freq(t, 12 / PHI) * period(t, 11 * PHI);
 
-	double s3 = 0;
+	double s = s1 * period(t, BIG_P, 0) + s2 * period(t, BIG_P, 1 / PHI);
 
-	s3 += tone(t, 480) * period(t, 24);
-	s3 += tone(t, 510) * period(t, 31);
-
-	double s = s1 * period(t, 120, 0) + s2 * period(t, 120, .33) + s3 * period(t, 120, .66);
-
-	s /= 16;
+	s /= 8;
 	assert(-1 <= s && s <= 1);
 
 	return s; 
