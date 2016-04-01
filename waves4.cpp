f06f175a31e9bc914e034fae5cd4cdb0be10c8f4
David Turner <susbdt@gmail.com>
2016-03-31 19:01:51 -0700

    X

--- waves4.cpp
+++ waves4.cpp
@@ -5,31 +5,44 @@
 void init() {
 	rampUpTime = 0.5;
 	rampDownTime = 1.5;
-	duration = 1 * 60;
+	duration = 28 * 60;
 }
 
 double wave(double t, double f, double waver, double p, double h = 0) {
-	double s = 0;
-	s += tone(t, f) * freq(t, waver) * period(t, p, h);
-	s += tone(t, f * PHI) * freq(t, waver * PHI) * period(t, p / 2, h);
-	return s;
+	return tone(t, f) * freq(t, waver) * period(t, p, h);
 }
 
 double sample(double t) {
-	double s = 0;
 
-	s += tone(t, 140      ) * freq(t, 6) * period(t, 10, 0.00);
-	s += tone(t, 140 * PHI) * freq(t, 6) * period(t, 20, 0.00);
+	double s0 = 0;
+	double F = 100;
+	double f = 1.2;
+	double W = 14;
+	double w = 0.98;
+	double P = 10;
+	double p = 1.4;
+	double H = 1;
+	double h = .95;
+	int I = 10;
+	for (int i = 0; i < I; ++i) {
+		s0 += wave(t, F * pow(f, i), W * pow(w, i), P * pow(p, i), H * pow(h, i));
+	}
 
-//	s += wave(t, 140, 6, 30, 0.25);
-//	s += wave(t, 143, 6, 30, 0.25);
-//	s += wave(t, 160, 9, 30, 0.00);
+	double s1 = 0;
+	double F1 = 120;
+	double f1 = 1.2;
+	double W1 = 8;
+	double w1 = 0.95;
+	double P1 = 14;
+	double p1 = 1.1;
+	double H1 = 1;
+	double h1 = .95;
+	int I1 = 6;
+	for (int i = 0; i < I; ++i) {
+		s0 += wave(t, F * pow(f, i), W * pow(w, i), P * pow(p, i), H * pow(h, i));
+	}
 
-//	s += wave(t, 155, 5, 90, 0.25);
-//	s += wave(t, 185, 5, 90, 0.00);
-
-//	s += wave(t, 200, 0.001, 170, 0.25);
-//	s += wave(t, 222, 0.002, 170, 0.00);
+	double s = s0 * period(t, duration * 2, .5) + s1 * period(t, duration * 2, 0);
 
 	s /= 16;
 	assert(-1 <= s && s <= 1);
