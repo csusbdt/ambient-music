565da312b708e69d333a5660684a384eb376f74d
David Turner <csusbdt@gmail.com>
2017-03-26 18:08:29 -0700

    X

--- third/Kronos.cpp
+++ third/Kronos.cpp
@@ -8,11 +8,9 @@
 Kronos::Kronos() : Sound(0, 120) {
 	waves.push_back(new Wave1( 0, 120));
 	waves.push_back(new Wave2(20, 100));
-/*
 	waves.push_back(new Wave3(40,  80));
 	waves.push_back(new Wave4(60,  60));
 	waves.push_back(new Wave5(80,  40));
-*/
 }
 
 double Kronos::sample(double t) const {
@@ -20,7 +18,7 @@ double Kronos::sample(double t) const {
 	for (int i = 0; i < waves.size(); ++i) {
 		s += waves[i]->rampedSample(t);
 	}
-	s /= 8;
+	s /= 4;
 	return s;
 }
 
