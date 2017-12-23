1a03f9f2b57336b6bb88b22b93c031667c4466f1
David Turner <csusbdt@gmail.com>
2017-12-22 18:48:35 -0800

    X

--- fifth/Kronos.cpp
+++ fifth/Kronos.cpp
@@ -9,7 +9,8 @@ Kronos::Kronos() : Sound() {
 	double intervalDuration = 240; 
 
 	startTime = 0;
-	duration = 20 * intervalDuration;
+	//duration = 20 * intervalDuration;
+	duration = 16 * intervalDuration;
 	attack = .5;
 	decay = 0;
 	release = 12;
@@ -23,28 +24,28 @@ Kronos::Kronos() : Sound() {
 	waves.push_back(new Wave2(s - 12, d + 24, 12, 12)); s += d;
 	waves.push_back(new Wave3(s - 12, d + 24, 12, 12)); s += d;
 	waves.push_back(new Wave4(s - 12, d + 24, 12, 12)); s += d;
-	waves.push_back(new Wave5(s - 12, d + 24, 12, 12)); s += d;
+//	waves.push_back(new Wave5(s - 12, d + 24, 12, 12)); s += d;
 
 	// overlap: 30s
 	waves.push_back(new Wave1(s - 12, d + 42, 12, 30)); s += d; 
 	waves.push_back(new Wave2(s - 30, d + 60, 30, 30)); s += d;
 	waves.push_back(new Wave3(s - 30, d + 60, 30, 30)); s += d;
 	waves.push_back(new Wave4(s - 30, d + 60, 30, 30)); s += d;
-	waves.push_back(new Wave5(s - 30, d + 60, 30, 30)); s += d;
+//	waves.push_back(new Wave5(s - 30, d + 60, 30, 30)); s += d;
 
 	// overlap: 60s
 	waves.push_back(new Wave1(s - 30, d +  90, 30, 60)); s += d; 
 	waves.push_back(new Wave2(s - 60, d + 120, 60, 60)); s += d;
 	waves.push_back(new Wave3(s - 60, d + 120, 60, 60)); s += d;
 	waves.push_back(new Wave4(s - 60, d + 120, 60, 60)); s += d;
-	waves.push_back(new Wave5(s - 60, d + 120, 60, 60)); s += d;
+//	waves.push_back(new Wave5(s - 60, d + 120, 60, 60)); s += d;
 
 	// overlap: 120s
 	waves.push_back(new Wave1(s -  60, d + 180,  60, 120)); s += d; 
 	waves.push_back(new Wave2(s - 120, d + 240, 120, 120)); s += d;
 	waves.push_back(new Wave3(s - 120, d + 240, 120, 120)); s += d;
 	waves.push_back(new Wave4(s - 120, d + 240, 120, 120)); s += d;
-	waves.push_back(new Wave5(s - 120, d + 120, 120,  12)); s += d;
+//	waves.push_back(new Wave5(s - 120, d + 120, 120,  12)); s += d;
 }
 
 double Kronos::sample(double clockTime) const {
